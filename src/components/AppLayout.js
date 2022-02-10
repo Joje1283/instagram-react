7796e4989d07f97ce5a6af8346d5e955ce372d11
Jaesik <joje1283@gmail.com>
2022-02-11 01:34:25 +0900

    AppLayout 설계 및 작업

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./AppLayout.scss"
 
 export default function AppLayout({children}) {
   return (
-    <div>AppLayout, {children}</div>
+    <div className="app">
+      <div className="header">
+        <div className="page-title">Instagram</div>
+        <div className="search">
+          <input type="text"/>
+        </div>
+        <div className="topnav">
+          메뉴1 메뉴2 메뉴3
+        </div>
+      </div>
+      <div className="contents">{children}</div>
+      <div className="sidebar">sidebar...</div>
+      <div className="footer">
+        &copy; 2022. Jaesik
+      </div>
+    </div>
   )
 }
